0be05cabaa13228cfa9e9ef0e7439e2aa3ee34cb
Ghostcrafter090 <Joshpumpkinhead@icloud.com>
2022-09-20 17:27:37 -0300

    Quickened voicemeeter check for vm plugin.

--- api/voicemeeter.py
+++ api/voicemeeter.py
@@ -15,7 +15,7 @@ def main():
         if ("voicemeeter8.exe" in (p.name() for p in psutil.process_iter())) == False:
             os.system("taskkill /f /im voicemeeter8.exe")
             os.system('start /b "" ..\..\Voicemeeter.lnk')
-        time.sleep(60)
+        time.sleep(5)
         status.vars['lastLoop'] = pytools.clock.getDateTime()
         status.finishedLoop = True
 
