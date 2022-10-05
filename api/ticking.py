0e7b2a198bfdb0fb1feed71e459ecbfd78b36f4e
Ghostcrafter090 <Joshpumpkinhead@icloud.com>
2022-10-04 23:04:58 -0300

    Minor Changes.

--- api/ticking.py
+++ api/ticking.py
@@ -17,9 +17,9 @@ def main():
     while True:
         dateArray = pytools.clock.getDateTime()
         if dateArray[4] == diaa:
-            pytools.sound.main.playSound("ticking.mp3", 0, 100, 1.0, 0.0, 0)
+            pytools.sound.main.playSound("ticking.mp3", 0, 100, 1.0, 0.0, 0, clock=False)
         if dateArray[4] == diab:
-            pytools.sound.main.playSound("ticking.mp3", 0, 100, 1.0, 0.0, 0)
+            pytools.sound.main.playSound("ticking.mp3", 0, 100, 1.0, 0.0, 0, clock=False)
         time.sleep(60)
         status.vars['lastLoop'] = pytools.clock.getDateTime()
         status.finishedLoop = True
